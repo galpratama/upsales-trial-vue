fd24e4995d45806812bc7734fc6e22125368ce31
Galih Pratama <hanamura.iost@gmail.com>
2023-06-06 11:43:41 +0700

    Dashboard View (exclude chart)

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,7 @@ import HomeView from '@/views/HomeView.vue'
 import SignUpViewVue from '@/views/Auth/SignUpView.vue'
 import AddProductView from '@/views/Auth/AddProductView.vue';
 import AddProductPhotoView from '@/views/Auth/AddProductPhotoView.vue';
+import DashboardView from '@/views/Dashboard/DashboardView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -26,7 +27,12 @@ const router = createRouter({
         path: '/add-product-photo',
         name: 'add-product-photo',
         component: AddProductPhotoView
-    }
+      },
+    {
+        path: '/dashboard',
+        name: 'dashboard',
+        component: DashboardView
+    },
   ]
 })
 
